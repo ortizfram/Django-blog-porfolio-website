26a347acbb9dd7b83231e2eecfd81e7a6130c396
melon <ortizfranco48@gmail.com>
2023-07-18 16:42:09 -0300

    markdown installed app above

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'markdownx',
     "blog",
     "portfolio",
-    'markdownx',
 ]
 
 MIDDLEWARE = [
