8915e6396fcef9cbd26a2c6d0df7bf1b7bcde8af
melon <ortizfranco48@gmail.com>
2023-07-18 16:33:07 -0300

    markdown description in blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from markdownx.models import MarkdownxField
 import datetime
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description =  MarkdownxField()
     image = models.ImageField(upload_to="blog/images")
-    date = models.DateField(datetime.date.today)
+    date = models.DateField(default=datetime.date.today)
