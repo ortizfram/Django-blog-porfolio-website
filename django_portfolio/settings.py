cb989c46a76c8a3e11add1d2a226a2e772f4e229
ortizfram <ortizfranco48@gmail.com>
2023-07-18 13:03:48 -0300

    allowed hosts

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "https://django-blog-porfolio-website-production.up.railway.app/"]
 
 
 # Application definition
