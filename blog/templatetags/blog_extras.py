ace5b21bb90e26bc3a563d0f069d738ff10d6410
ortizfram <ortizfranco48@gmail.com>
2023-07-19 12:55:06 -0300

    markdown highlighted

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,11 +2,25 @@ import markdown
 
 from django import template
 from django.template.defaultfilters import stringfilter
+import markdown.extensions.codehilite
+import markdown.extensions.fenced_code
 
 register = template.Library()
 
 
 @register.filter
 @stringfilter
-def convert_markdown(value):
-    return markdown.markdown(value, extensions={"markdown.extensions.fenced_code"})
+def convert_markdown_with_highlight(value):
+    return markdown.markdown(
+        value,
+        extensions=[
+            "markdown.extensions.fenced_code",
+            "markdown.extensions.codehilite",  # Enable code highlighting
+        ],
+        extension_configs={
+            "markdown.extensions.codehilite": {
+                "linenums": True,  # Add line numbers (optional)
+                "guess_lang": False,  # Do not guess the language
+            }
+        },
+    )
